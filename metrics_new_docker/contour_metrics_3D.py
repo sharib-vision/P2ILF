833ebc8ca827435b696450bbcfb3e9451a596c6b
Sharib Ali <38819666+sharib-vision@users.noreply.github.com>
2022-09-12 17:18:46 +0100

    tackle nan for no ligament in GT/pred

--- metrics_new_docker/contour_metrics_3D.py
+++ metrics_new_docker/contour_metrics_3D.py
@@ -24,9 +24,9 @@ def Hausdorff_dist(vol_a,vol_b):
 def get_args():
     import argparse
     parser = argparse.ArgumentParser(description="3D contour distance metric - task 1 @ P2ILF challenge @ MICCAI 2022", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--ModelRef", type=str, default="/Users/sharib/Documents/docker_evaluation/evaluationCodes/3D_metrics/testx/patient4_3D-liver-model.obj", help="Original 3D model - GT reference model")
-    parser.add_argument("--ridgeLigamentLandmarks_3D_GT", type=str, default="/Users/sharib/Documents/docker_evaluation/evaluationCodes/3D_metrics/testx/patient4_3_3D-contours_GT.json", help="2D and 3D contours")
-    parser.add_argument("--ridgeLigamentLandmarks_3D_eval", type=str, default="/Users/sharib/Documents/docker_evaluation/evaluationCodes/3D_metrics/testx/patient4_3_3D-contours_pred.json", help="2D and 3D contours")
+    parser.add_argument("--ModelRef", type=str, default="/Users/sharib/Documents/docker_evaluation/GT/patient11_3D-liver-model.obj", help="Original 3D model - GT reference model")
+    parser.add_argument("--ridgeLigamentLandmarks_3D_GT", type=str, default="/Users/sharib/Documents/docker_evaluation/GT/patient11_2_3D-contours.json", help="2D and 3D contours")
+    parser.add_argument("--ridgeLigamentLandmarks_3D_eval", type=str, default="/Users/sharib/Documents/docker_evaluation/pred/3d-liver-contours.json", help="2D and 3D contours")
     parser.add_argument("--outputFileName", type=str, default="./output/metric_3D.json", help="2D and 3D contours")
     args = parser.parse_args()
     return args
@@ -137,50 +137,57 @@ def metric3Dcompute(ModelRef, contours_3D_GT, contours_3D_eval):
             
         except:
             print("Ridge not found in either GT or predicted data")
-        
-        if (ctypeD_names[idx]=='Ligament'):
+          
             
-            try:
-                # Concatenate ligaments contours for GT and eval data:
+        try:
+            lig_flag = 0
+            for idx in range(0, len(ctypeD_names)):
                 cords3D_ligament_gt = []
                 cords3D_ligament_eval = []
-                for idx in range(0, len(ctypeD_names)):
-                    if (ctypeD_names[idx]=='Ligament'):
-                        vertex3D_GT = vertices_GT_contour[idx]
-                        for k in range(0, len(vertex3D_GT)):
-                            cords3D_ligament_gt.append(vertices_GT[vertex3D_GT[k]])
-                
-                for idx in range(0, len(ctypeD_names_eval)):
-                    if (ctypeD_names_eval[idx]=='Ligament'):
-                        vertex3D_eval = vertices_eval_contour[idx]
-                        for k in range(0, len(vertex3D_eval)):
-                            cords3D_ligament_eval.append(vertices_GT[vertex3D_eval[k]])
-                            
-                # find distance from nearest neighbor in GT
-                pcd.points = o3d.utility.Vector3dVector(cords3D_ligament_gt)
-                distances_GT = pcd.compute_nearest_neighbor_distance()
-                avg_dist_GT = np.mean(distances_GT)
-                # print(avg_dist_GT)
-                # o3d.visualization.draw_geometries([pcd])
-                
-                # find distance from nearest neighbor in predicted
-                pcd = o3d.geometry.PointCloud()   
-                pcd.points = o3d.utility.Vector3dVector(cords3D_ligament_eval)
-                # o3d.visualization.draw_geometries([pcd])
-                distances_eval = pcd.compute_nearest_neighbor_distance()
-                avg_dist_eval = np.mean(distances_eval)
-                # print(avg_dist_eval)
-                
-                # Note: the distance difference will penalise the score : min -> best (ideally 0)
-                distNN_diff = np.abs(np.asarray(avg_dist_eval)-np.asarray(avg_dist_GT))
-                dist_NN.append(distNN_diff)
-                print(distNN_diff)
-                valHFD = Hausdorff_dist(cords3D_ligament_gt, cords3D_ligament_eval)
-                dist_HFD.append(valHFD)
-            
-            except:
-                print("Ligament not found in either GT or predicted data")
+                # This will execute only if ligament is present in the GT
+                if (ctypeD_names[idx]=='Ligament'):
+                    # Concatenate ligaments contours for GT and eval data:
+                    # for idx in range(0, len(ctypeD_names)):
+
+                    vertex3D_GT = vertices_GT_contour[idx]
                     
+                    for k in range(0, len(vertex3D_GT)):
+                        cords3D_ligament_gt.append(vertices_GT[vertex3D_GT[k]])
+                    
+                    for idx in range(0, len(ctypeD_names_eval)):
+                        if (ctypeD_names_eval[idx]=='Ligament'):
+                            lig_flag = 1
+                            vertex3D_eval = vertices_eval_contour[idx]
+                            for k in range(0, len(vertex3D_eval)):
+                                cords3D_ligament_eval.append(vertices_GT[vertex3D_eval[k]])
+
+                    # Deals with nan --> we take these into account only if both GT and prediction have this -- 
+                    if lig_flag:      
+                        # find distance from nearest neighbor in GT
+                        pcd.points = o3d.utility.Vector3dVector(cords3D_ligament_gt)
+                        distances_GT = pcd.compute_nearest_neighbor_distance()
+                        avg_dist_GT = np.mean(distances_GT)
+                        # print(avg_dist_GT)
+                        # o3d.visualization.draw_geometries([pcd])
+                        
+                        # find distance from nearest neighbor in predicted
+                        pcd = o3d.geometry.PointCloud()   
+                        pcd.points = o3d.utility.Vector3dVector(cords3D_ligament_eval)
+                        # o3d.visualization.draw_geometries([pcd])
+                        distances_eval = pcd.compute_nearest_neighbor_distance()
+                        avg_dist_eval = np.mean(distances_eval)
+                        # print(avg_dist_eval)
+                        
+                        # Note: the distance difference will penalise the score : min -> best (ideally 0)
+                        distNN_diff = np.abs(np.asarray(avg_dist_eval)-np.asarray(avg_dist_GT))
+                        dist_NN.append(distNN_diff)
+                        print(distNN_diff)
+                        valHFD = Hausdorff_dist(cords3D_ligament_gt, cords3D_ligament_eval)
+                        dist_HFD.append(valHFD)
+                
+        except:
+            print("Ligament not found in either GT or predicted data")
+                
         print(dist_HFD) 
         print(distNN_diff)
         return np.mean(dist_NN).astype('float'), np.mean(dist_HFD).astype('float')
